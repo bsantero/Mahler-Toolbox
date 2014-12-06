d357f870a0d49c1292615bf274b5ca0abf970c9b
bsantero <brian.santero@gmail.com>
2014-12-06 00:23:38 -0600

    Fix commit

--- js/application.js
+++ js/application.js
@@ -2,6 +2,7 @@ var settings = {};
 settings.currentPitch = "3";
 settings.notes = ["4A","4Asharp","4B","5C","5Csharp","5D","5Dsharp","5E","5F","5Fsharp","5G","5Gsharp"];
 settings.soundPack = [];
+settings.isPlaying = false;
 settings.rebuildSoundPack = function(scale, startNote) {
 };
 
@@ -10,44 +11,54 @@ function createSoundPack() {
     settings.soundPack[soundIndex] = new Howl({
       urls: ['media/5seconds/OGG/'+settings.notes[soundIndex]+'.ogg', 'media/5seconds/MP3'+settings.notes[soundIndex]+'.mp3'],
       volume: .5,
-      loop: true
+      loop: true 
     });
   }
+  //console.log(settings.soundPack);
 }
 
-function pitchChange(el) {
-  var el = this;
-  var direction = $(this).data("direction"),
+function pitchChange(details) {
+  var el = this,
+      direction = $(this).data("direction"),
       scale = settings.notes;
-      //pitchDisplay = $(this).parent().find("#droneInput").find("input"),
-      //pitchDisplayIndex = scale.indexOf(pitchDisplay.val()); 
-  console.log('Current pitch is' + settings.currentPitch + " - " + scale[settings.currentPitch]);
+  console.log('Current pitch is ' + settings.currentPitch + " - " + scale[settings.currentPitch]);
   if (direction === "down") {
     if (settings.currentPitch === 0) {
       settings.currentPitch = scale.length-1;
     }
     else {
-      settings.currentPitch -= 1;
+      settings.currentPitch--;
     }
   }
-  else {
-    if (setting.currentPitch === scale.length-1) {
+  else if (direction === "up") {
+    if (settings.currentPitch === scale.length-1) {
       settings.currentPitch = 0;
     }
     else {
-      settings.currentPitch += 1;
+      settings.currentPitch++;
     }
   }
-  console.log('Next pitch is' + settings.currentPitch + " - " + scale[settings.currentPitch]);
+  console.log('Next pitch is ' + settings.currentPitch + " - " + scale[settings.currentPitch]);
   $(el).parent().find("#droneInput").find("input").val(settings.notes[settings.currentPitch]);
-  playPitch();
+  if (settings.isPlaying === true){
+    playPitch();
+  }
 }
 
 function playPitch() {
-  console.log("Should play "+settings.notes[settings.currentPitch]);
+  settings.soundPack[settings.currentPitch].stop();
+  settings.isPlaying = true;
+  console.log("Should play "+settings.soundPack[settings.currentPitch]);
   settings.soundPack[settings.currentPitch].play();
-  //$.delay(30);
-  settings.soundPack[settings.currentPitch].play({"fadeIn":30});
+  $(this).hide();
+  $(this).siblings("#stop").show();
+}
+
+function stopPitch() {
+  settings.isPlaying = false;
+  settings.soundPack[settings.currentPitch].stop();
+  $(this).hide();
+  $(this).siblings("#play").show();
 }
 
 $(document).ready(function() {
@@ -56,5 +67,7 @@ $(document).ready(function() {
   $(".changePitch.down").on("click", pitchChange);
   $(".changePitch.up").on("click", pitchChange);
   $("#play").on("click", playPitch);
+  $("#stop").on("click", stopPitch);
+  console.log(settings.currentPitch);
 
 });
