d760dc0c60f54b585a1304dc8a686c2d0543b9c5
bsantero <brian.santero@gmail.com>
2014-12-20 13:53:58 -0600

    Fully functional oscillation, 3 minute delay on Safari

--- js/oscillator.js
+++ js/oscillator.js
@@ -1,5 +1,4 @@
-var settings = {},
-  context = new AudioContext();
+var settings = {};
 settings.currentPitch = {"name": "A", "octave":4, "frequency": 440};
 settings.notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
 settings.isPlaying = false;
@@ -16,55 +15,73 @@ function calcFreq(direction) {
 
 function pitchChange(details) {
   var direction = $(this).data("direction");
+  if ((direction === "up" && settings.currentPitch.frequency * Math.pow(1.059463094359, 1) >= 1700) || (direction === "down" && settings.currentPitch.frequency * Math.pow(1.059463094359, -1) <= 50 )) return;
   switch (settings.currentPitch.name) {
     case "A":
-      direction === "up" ? settings.currentPitch.name = "A#" : settings.currentPitch.name = "G#";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "A#";
+      } else settings.currentPitch.name = "G#";
+      break;
     case "A#":
-      direction === "up" ? settings.currentPitch.name = "B" : settings.currentPitch.name = "A";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "B";
+      } else settings.currentPitch.name = "A";
+      break;
     case "B":
       if (direction === "up") {
         settings.currentPitch.name = "C";
         settings.currentPitch.octave++;
       } else settings.currentPitch.name = "A#";
-      break
+      break;
     case "C":
       if (direction === "up") {settings.currentPitch.name = "C#";} else {
         settings.currentPitch.name = "B";
         settings.currentPitch.octave--;
       }
-      break
+      break;
     case "C#":
-      direction === "up" ? settings.currentPitch.name = "D" : settings.currentPitch.name = "C";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "D";
+      } else settings.currentPitch.name = "C";
+      break;
     case "D":
-      direction === "up" ? settings.currentPitch.name = "D#" : settings.currentPitch.name = "C#";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "D#";
+      } else settings.currentPitch.name = "C#";
+      break;
     case "D#":
-      direction === "up" ? settings.currentPitch.name = "E" : settings.currentPitch.name = "D";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "E";
+      } else settings.currentPitch.name = "D";
+      break;
     case "E":
-      direction === "up" ? settings.currentPitch.name = "F" : settings.currentPitch.name = "D#";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "F";
+      } else settings.currentPitch.name = "D#";
+      break;
     case "F":
-      direction === "up" ? settings.currentPitch.name = "F#" : settings.currentPitch.name = "E";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "F#";
+      } else settings.currentPitch.name = "E";
+      break;
     case "F#":
-      direction === "up" ? settings.currentPitch.name = "G" : settings.currentPitch.name = "F";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "G";
+      } else settings.currentPitch.name = "F";
+      break;
     case "G":
-      direction === "up" ? settings.currentPitch.name = "G#" : settings.currentPitch.name = "F#";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "G#";
+      } else settings.currentPitch.name = "F#";
+      break;
     case "G#":
-      direction === "up" ? settings.currentPitch.name = "A" : settings.currentPitch.name = "G";
-      break
+      if (direction === "up") {
+        settings.currentPitch.name = "A";
+      } else settings.currentPitch.name = "G";
+      break;
   }
 
   calcFreq(direction);
-
-
-
 
   $("span.display.pitch").html(settings.currentPitch.name);
   $("span.display.octave").html(settings.currentPitch.octave);
@@ -72,30 +89,40 @@ function pitchChange(details) {
 
 function newOsc() {
   oscillator = context.createOscillator(); // Create bass guitar
-  gainNode = context.createGain(); // Create boost pedal 
-  oscillator.connect(gainNode); // Connect bass guitar to boost pedal
-  gainNode.connect(context.destination); // Connect boost pedal to amplifier
-  oscillator.frequency.value = settings.currentPitch.frequency;
-  gainNode.gain.value = .5; // Set boost pedal to 30 percent volume
+  //gainNode = context.createGain(); // Create boost pedal 
+  oscillator.connect(context.destination); // Connect bass guitar to boost pedal
+  //oscillator.connect(gainNode); // Connect bass guitar to boost pedal
+  //gainNode.connect(context.destination); // Connect boost pedal to amplifier
+  //gainNode.gain.value = 1; // Set boost pedal to 30 percent volume
 }
 
 function playPitch() {
   newOsc();
   console.log("playing "+settings.currentPitch.frequency);
-  oscillator.start();
+  if (window.AudioContext) {
+    oscillator.frequency.value = settings.currentPitch.frequency;
+    oscillator.start();
+  } else {
+    oscillator.start(settings.currentPitch.frequency);
+  }
   settings.isPlaying = true;
   $(this).hide();
   $(this).siblings("#stop").show();
 }
 
 function stopPitch() {
-  oscillator.stop();
+  if (window.AudioContext) {
+    oscillator.stop();
+  } else {
+    oscillator.stop(settings.currentPitch.frequency);
+  }
   settings.isPlaying = false;
   $(this).hide();
   $(this).siblings("#play").show();
 }
 
 $(document).ready(function() {
+  context = new (window.AudioContext || window.webkitAudioContext)();
 
   $(".changePitch").on("click", pitchChange);
   $("#play").on("click", playPitch);
