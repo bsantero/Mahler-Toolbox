7d7818c88f1155f5ebae8fb00983e7640f709eb1
bsantero <brian.santero@gmail.com>
2014-12-20 15:46:47 -0600

    Keyboard Shortcuts, User defined settings, less suck

--- js/oscillator.js
+++ js/oscillator.js
@@ -1,99 +1,107 @@
 var settings = {};
-settings.currentPitch = {"name": "A", "octave":4, "frequency": 440};
-settings.notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
+settings.currentPitch = {"name": "A", "octave": 4, "frequency": 440};
+settings.userDef = {"name": "A", "octave": 4, "frequency": 440};
 settings.isPlaying = false;
+//settings.keydir = null;
 
-function calcFreq(direction) {
-  var freq = settings.currentPitch.frequency,
-    exponent;
-  if (direction === "up") exponent = 1;
-  else if (direction === "down") exponent = -1;
-  settings.currentPitch.frequency = settings.currentPitch.frequency * Math.pow(1.059463094359, exponent);
-  oscillator.frequency.value = settings.currentPitch.frequency;
-  $("span.display.frequency").html(settings.currentPitch.frequency);
+function calcFreq(direction, reset) {
+  var pitch = settings.currentPitch,
+    userPitch = settings.userDef;
+    exponent = undefined;
+  if (reset) {
+    pitch.name = userPitch.name;
+    pitch.octave = userPitch.octave;
+    pitch.frequency = userPitch.frequency;
+    $("span.display.pitch").html(pitch.name);
+    $("span.display.octave").html(pitch.octave);
+  } else {
+    if (direction === "up") exponent = 1; else if (direction === "down") exponent = -1;
+    pitch.frequency = pitch.frequency * Math.pow(1.059463094359, exponent);
+  }
+  oscillator.frequency.value = pitch.frequency;
+  $("span.display.frequency").html(pitch.frequency);
 }
 
-function pitchChange(details) {
-  var direction = $(this).data("direction");
-  if ((direction === "up" && settings.currentPitch.frequency * Math.pow(1.059463094359, 1) >= 1700) || (direction === "down" && settings.currentPitch.frequency * Math.pow(1.059463094359, -1) <= 50 )) return;
-  switch (settings.currentPitch.name) {
+function pitchChange(keydir) {
+  var direction = keydir || $(this).data("direction"),
+    pitch = settings.currentPitch;
+  if ((direction === "up" && pitch.frequency * Math.pow(1.059463094359, 1) >= 1761) || (direction === "down" && pitch.frequency * Math.pow(1.059463094359, -1) <= 54 )) return;
+  switch (pitch.name) {
     case "A":
       if (direction === "up") {
-        settings.currentPitch.name = "A#";
-      } else settings.currentPitch.name = "G#";
+        pitch.name = "A#";
+      } else pitch.name = "G#";
       break;
     case "A#":
       if (direction === "up") {
-        settings.currentPitch.name = "B";
-      } else settings.currentPitch.name = "A";
+        pitch.name = "B";
+      } else pitch.name = "A";
       break;
     case "B":
       if (direction === "up") {
-        settings.currentPitch.name = "C";
-        settings.currentPitch.octave++;
-      } else settings.currentPitch.name = "A#";
+        pitch.name = "C";
+        pitch.octave++;
+      } else pitch.name = "A#";
       break;
     case "C":
-      if (direction === "up") {settings.currentPitch.name = "C#";} else {
-        settings.currentPitch.name = "B";
-        settings.currentPitch.octave--;
+      if (direction === "up") {pitch.name = "C#";} else {
+        pitch.name = "B";
+        pitch.octave--;
       }
       break;
     case "C#":
       if (direction === "up") {
-        settings.currentPitch.name = "D";
-      } else settings.currentPitch.name = "C";
+        pitch.name = "D";
+      } else pitch.name = "C";
       break;
     case "D":
       if (direction === "up") {
-        settings.currentPitch.name = "D#";
-      } else settings.currentPitch.name = "C#";
+        pitch.name = "D#";
+      } else pitch.name = "C#";
       break;
     case "D#":
       if (direction === "up") {
-        settings.currentPitch.name = "E";
-      } else settings.currentPitch.name = "D";
+        pitch.name = "E";
+      } else pitch.name = "D";
       break;
     case "E":
       if (direction === "up") {
-        settings.currentPitch.name = "F";
-      } else settings.currentPitch.name = "D#";
+        pitch.name = "F";
+      } else pitch.name = "D#";
       break;
     case "F":
       if (direction === "up") {
-        settings.currentPitch.name = "F#";
-      } else settings.currentPitch.name = "E";
+        pitch.name = "F#";
+      } else pitch.name = "E";
       break;
     case "F#":
       if (direction === "up") {
-        settings.currentPitch.name = "G";
-      } else settings.currentPitch.name = "F";
+        pitch.name = "G";
+      } else pitch.name = "F";
       break;
     case "G":
       if (direction === "up") {
-        settings.currentPitch.name = "G#";
-      } else settings.currentPitch.name = "F#";
+        pitch.name = "G#";
+      } else pitch.name = "F#";
       break;
     case "G#":
       if (direction === "up") {
-        settings.currentPitch.name = "A";
-      } else settings.currentPitch.name = "G";
+        pitch.name = "A";
+      } else pitch.name = "G";
       break;
   }
-
   calcFreq(direction);
-
-  $("span.display.pitch").html(settings.currentPitch.name);
-  $("span.display.octave").html(settings.currentPitch.octave);
+  $("span.display.pitch").html(pitch.name);
+  $("span.display.octave").html(pitch.octave);
+  settings.keydir = null;
 }
 
 function newOsc() {
   oscillator = context.createOscillator(); // Create bass guitar
-  //gainNode = context.createGain(); // Create boost pedal 
-  oscillator.connect(context.destination); // Connect bass guitar to boost pedal
-  //oscillator.connect(gainNode); // Connect bass guitar to boost pedal
-  //gainNode.connect(context.destination); // Connect boost pedal to amplifier
-  //gainNode.gain.value = 1; // Set boost pedal to 30 percent volume
+  gainNode = context.createGain(); // Create boost pedal 
+  oscillator.connect(gainNode); // Connect bass guitar to boost pedal
+  gainNode.connect(context.destination); // Connect boost pedal to amplifier
+  gainNode.gain.value = 1; // Set boost pedal to 30 percent volume
 }
 
 function playPitch() {
@@ -106,8 +114,8 @@ function playPitch() {
     oscillator.start(settings.currentPitch.frequency);
   }
   settings.isPlaying = true;
-  $(this).hide();
-  $(this).siblings("#stop").show();
+  $("button#play").hide();
+  $("button#stop").show();
 }
 
 function stopPitch() {
@@ -117,17 +125,54 @@ function stopPitch() {
     oscillator.stop(settings.currentPitch.frequency);
   }
   settings.isPlaying = false;
-  $(this).hide();
-  $(this).siblings("#play").show();
+  $("button#stop").hide();
+  $("button#play").show();
+}
+
+function setUserDefault(definedPitch){
+  if (definedPitch) {
+  } else {
+    console.log("Default set to: " + settings.currentPitch.name + settings.currentPitch.octave);
+    settings.userDef.name = settings.currentPitch.name;
+    settings.userDef.octave = settings.currentPitch.octave;
+    settings.userDef.frequency = settings.currentPitch.frequency;
+  }
 }
 
 $(document).ready(function() {
-  context = new (window.AudioContext || window.webkitAudioContext)();
 
-  $(".changePitch").on("click", pitchChange);
   $("#play").on("click", playPitch);
   $("#stop").on("click", stopPitch);
-  console.log("pitch: "+settings.currentPitch.name);
+  $("#reset").on("click", function(){ calcFreq(null, true); });
+  $('#default').on("click", function(){
+    setUserDefault();
+  });
+
+
+  $("#droneUp").on("click", function(){
+    pitchChange("up");
+  });
+  $("#droneDown").on("click", function(){
+    pitchChange("down");
+  });
+  Mousetrap.bind(['q','w','e','r','t','a','s','d','f','g','z','x','c','v'], function(){
+    //settings.keydir = "up";
+    pitchChange("up");
+  });
+  Mousetrap.bind(['y','u','i','o','p','h','j','k','l','b','n','m'], function(){ 
+    //settings.keydir = "down";
+    pitchChange("down");
+  });
+  Mousetrap.bind('space', function(){
+    if (settings.isPlaying) stopPitch(); else playPitch();
+  });
+  Mousetrap.bind('shift', function(){
+    calcFreq(null, true);
+  });
+
+  context = new (window.AudioContext || window.webkitAudioContext)();
+
   newOsc();
+
 
 });
